1c6e799e8011212146e7fcd6836dff7ef99665ea
ValentinaGenty <jxgenty@gmail.com>
2023-11-06 23:07:12 -0600

    almostt

--- myApp.js
+++ myApp.js
@@ -11,14 +11,17 @@ app.get("/",(req,res)=>{
 
 stylePath=__dirname+"/public";
 app.use("/public",express.static(stylePath));
-let message="Hello json";
+
+
+
+app.get("/json",(req,res)=>{
+    let message="Hello json";
 if(process.env.MESSAGE_STYLE==="uppercase"){
     message="HELLO JSON";
-}
-app.get("/json",(req,res)=>{
+};
         res.json({
             "message":message
-        })
+        });
 })
 
 
