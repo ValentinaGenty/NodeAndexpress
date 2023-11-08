9299e3efa4261e5cff6a304f19654152470a09b4
ValentinaGenty <jxgenty@gmail.com>
2023-11-07 19:33:57 -0600

    chain middle ware with a final handler

--- myApp.js
+++ myApp.js
@@ -12,13 +12,20 @@ app.get("/",(req,res)=>{
 stylePath=__dirname+"/public";
 app.use("/public",express.static(stylePath));
 
-
-
 app.use(function(req,res,next){
     let string=req.method+" "+req.path+" "+"-"+" "+req.ip;
     console.log(string);
     next();
 })
+
+app.get('/now',(req,res,next){
+    req.time=new Date().toString()
+    next();
+},(req,res){
+    res.send({
+        "time":req.time
+    });
+});
 
 
 
